bf6b8f5e49ecbd3c73dc763bae238fa6bfa68458
ehdrjs4802 <ehdrjs4802@gmail.com>
2024-12-23 20:12:31 +0900

    feat:merge

--- linkly/src/main/java/com/example/linkly/entity/Heart.java
+++ linkly/src/main/java/com/example/linkly/entity/Heart.java
@@ -2,24 +2,27 @@ package com.example.linkly.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
 @Table(name = "heart")
 public class Heart {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+        @ManyToOne
+        @JoinColumn(name = "user_id")
+        @Setter
+        private User user;
 
-    @ManyToOne
-    @JoinColumn(name = "feed_id")
-    private Feed feed;
+        @ManyToOne
+        @JoinColumn(name = "feed_id")
+        @Setter
+        private Feed feed;
 
-    public Heart() {
-    }
+        public Heart() {
+        }
 }
