e8ff6ae73565dc43c8f6f33d03032a8343a43d9d
ehdrjs4802 <ehdrjs4802@gmail.com>
2024-12-26 17:09:22 +0900

    fix:merge dev into feed

--- linkly/src/main/java/com/example/linkly/entity/Feed.java
+++ linkly/src/main/java/com/example/linkly/entity/Feed.java
@@ -12,7 +12,6 @@ import java.time.LocalDateTime;
 @Getter
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "feed")
-@EntityListeners(AuditingEntityListener.class)
 public class Feed {
 
     @Id
