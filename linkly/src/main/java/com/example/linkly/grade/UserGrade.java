3c2852dcb93c9b20f8367e22a9081d1cc15f857b
sy <suyun1886@gmail.com>
2024-12-26 09:02:22 +0900

    fix: UserGrade 수정

--- linkly/src/main/java/com/example/linkly/grade/UserGrade.java
+++ linkly/src/main/java/com/example/linkly/grade/UserGrade.java
@@ -1,7 +1,5 @@
 package com.example.linkly.grade;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
 public enum UserGrade {
     BASIC("BASIC"),
     VIP("VIP");
@@ -12,9 +10,4 @@ public enum UserGrade {
         this.value = value;
     }
 
-    //대소문자 구분 x
-//    @JsonCreator
-//    public static UserGrade from(String value) {
-//        return UserGrade.valueOf(value.toUpperCase());
-//    }
 }
